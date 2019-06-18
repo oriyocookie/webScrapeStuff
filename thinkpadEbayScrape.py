b4e5dcd5b8868ceeea9fd7ac39af6ee95a5eab65
Oriyo Chakma <oriyochakma@gmail.com>
2019-06-18 14:06:46 +0600

    made the code more readable

--- thinkpadEbayScrape.py
+++ thinkpadEbayScrape.py
@@ -1,3 +1,4 @@
+# TODO: Implement most common words in name
 # This script just scrapes ebay looking for t440s thinkpad within a certain price range
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as bSoup
@@ -13,8 +14,8 @@ def conn(my_url):
     return pageSoup
 
 
-priceLookup = input("Please enter the price you are willing to pay max: ")
-print("You will be looking within $100-" + str(priceLookup) + " range")
+maxPrice = input("Please enter the price you are willing to pay max: ")
+print("You will be looking within $100-" + str(maxPrice) + " range")
 
 my_url1 = 'https://www.ebay.ca/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR12.TRC2.A0.H0.Xthinkpad+t440s.TRS0&_nkw=thinkpad+t440s&_sacat=0'
 
@@ -27,21 +28,23 @@ link = pageSoup.findAll("a", {"class": "vip"})
 price = pageSoup.findAll("li", {"class": "lvprice prc"})
 
 
-# print("number of items: " + str(len(price)))
+# print("number of items: " + str(totalItems))
 
 i = 0
 totalPrice = 0
 includedPrice = 0
 included = 0
 
-while i < len(price):
+totalItems = len(price)
+
+while i < totalItems:
     printName = name[i].a.text.strip()
     printLink = link[i].get('href')
     printPrice = price[i].span.text.replace(" ", "")
     slicePrice = float(re.search(r'\d+\.+\d+\d', printPrice).group())
     totalPrice += slicePrice
 
-    if slicePrice < float(priceLookup) and slicePrice > 100:
+    if slicePrice < float(maxPrice) and slicePrice > 100:
         print("name: " + printName)
         print("link: " + printLink)
         print("price: " + printPrice)
@@ -51,9 +54,9 @@ while i < len(price):
 
     i += 1
 
-avgPrice = totalPrice / len(price)
+avgPrice = totalPrice / totalItems
 includedPrice = includedPrice / included
 
-print("The average price of all " + str(len(price)) + " Thinkpads is: $" + str(avgPrice))
+print("The average price of all " + str(totalItems) + " Thinkpads is: $" + str(avgPrice))
 
 print("The average price of the listed " + str(included) + " Thinkpads is: $" + str(includedPrice))
